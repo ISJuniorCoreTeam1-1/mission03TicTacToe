1b7354415724c61d02771a09d8d0561f4cac10ac
Mitch McCoard <mitch.mccoard@gmail.com>
2023-01-23 09:31:00 -0700

    Hi

--- mission03TicTacToe/Program.cs
+++ mission03TicTacToe/Program.cs
@@ -6,7 +6,7 @@ namespace mission03TicTacToe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello firends!");
         }
     }
 }
