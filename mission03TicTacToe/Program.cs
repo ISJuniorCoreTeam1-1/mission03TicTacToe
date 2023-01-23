2e5376ee3e92e2bc0692c5cdc9473626c6b9be31
spencerrfree <sf376@byu.edu>
2023-01-23 11:28:24 -0700

    Spencer 1st Draft

--- mission03TicTacToe/Program.cs
+++ mission03TicTacToe/Program.cs
@@ -2,41 +2,71 @@
 
 namespace mission03TicTacToe
 {
-    class Program
+ 
+    class Driver
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello firends!");
-             //This is the array for the board to store answers, Spots 1-9 correlate with nine spots on board
+
+            //Create a new Game
+            Supporting myGame = new Supporting();
+            //This is the array for the board to store answers, Spots 1-9 correlate with nine spots on board
             char[] board = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-            int player = 1;
-           
+            //Welcome user to the game
             Console.WriteLine("Welcome to Tic-Tac-Toe!");
             Console.WriteLine("\n");
-           
-            if (player == 1) //Deciding whose turn it is
+            string selectedValue = "";
+            string str = "";
+            myGame.PrintBoard(board);
+            Console.WriteLine("Player 1: It's your turn. Enter the number on the board to place your X");
+            //Read In User Entry and update the array
+            str = Console.ReadLine();
+            selectedValue = str;
+            for (int i = 0; i < board.Length; i++)
             {
-                Console.WriteLine("Player 1: It's your turn");
+                if (board[i].ToString() == str)
+                {
+                    board[i] = 'X';
+                }
+            }
+            if (myGame.Winner() == 0){
+                for (int i = 0; i < 4; i++)
+                {
+                    myGame.PrintBoard(board);
+                    Console.WriteLine("Player 2: It's your turn. Enter the number on the board to place your X");
+                    //Read In User Entry and update the array
+                    str = Console.ReadLine();
+                    selectedValue = str;
+                    for (int i = 0; i < board.Length; i++)
+                    {
+                        if (board[i].ToString() == str)
+                        {
+                            board[i] = 'O';
+                        }
+                    }
+                }
+
+                for (int i = 0; i < 4; i++)
+                {
+                    myGame.PrintBoard(board);
+                    Console.WriteLine("Player 1: It's your turn. Enter the number on the board to place your X");
+                    //Read In User Entry and update the array
+                    str = Console.ReadLine();
+                    selectedValue = str;
+                    for (int i = 0; i < board.Length; i++)
+                    {
+                        if (board[i].ToString() == str)
+                        {
+                            board[i] = 'X';
+                        }
+                    }
+                }
             }
             else
             {
-                Console.WriteLine("Player 2: It's your turn");
+                Console.WriteLine(myGame.Winner);
             }
-           
-
-                Console.WriteLine("\n");
-               
-                //Tic-Tac-Toe board. Players will type in which spot they want their X or O in (1-9)
-                //It will store it in board array and change what spot they chose to either X or O
-                Console.WriteLine("    |    |    ");
-                Console.WriteLine("{0}   | {1}  | {2} ", board[1], board[2], board[3]);
-                Console.WriteLine("____|____|____");
-                Console.WriteLine("    |    |    ");
-                Console.WriteLine("{0}   | {1}  | {2} ", board[4], board[5], board[6]);
-                Console.WriteLine("____|____|____");
-                Console.WriteLine("    |    |    ");
-                Console.WriteLine("{0}   | {1}  | {2} ", board[7], board[8], board[9]);
-                Console.WriteLine("    |    |    ");
+            
         }
     }
 }
